cc1738a21d50e37a061efedcc94a21b43e1db97b
Miha Absec <miha.absec@gmail.com>
2025-01-23 18:08:55 +0100

    Lala

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <cstdlib>
+
+int main(int argumentCount, char * arguments[])
+{
+    SDL_Window * window = SDL_CreateWindow("SDL!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Delay(1000);
+    SDL_DestroyWindow(window);
+
+    return EXIT_SUCCESS;
+}
